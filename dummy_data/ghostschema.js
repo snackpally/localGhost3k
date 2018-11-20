2a4563ca24f8b6b1fb9b9fd5b8af1ca8b0dea3a0
Zack Viente <zviente@gmail.com>
2018-11-20 13:40:50 -0700

    development branch dummy data

---
+++ dummy_data/ghostschema.js
@@ -0,0 +1,13 @@
+const ghostSchema = {
+	"id": Number,
+	"ghost_name": String,
+	"place_name": String,
+	"ghost_imgs": String,
+	"ghost_type": String,
+	"loc": {lon: Number,
+	lat: Number},
+	"loc_type": String,
+	"loc_img_link": String,
+	"loc_desc": String,
+	"address": String
+};
