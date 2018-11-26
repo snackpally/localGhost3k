08206d50812171b57836b28ce0c127bcee32ea2b
Zack Viente <zviente@gmail.com>
2018-11-26 14:24:38 -0700

    add example of how to import components

---
+++ src/greeting.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+class Greeting extends React.Component {
+	render () {
+		return (
+			<p>This is the best damn Ghost app on the web</p>
+		)
+	}
+}
+
+export default Greeting 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import Greeting from './greeting';
 
 class HelloWorld extends React.Component {
 	render() {
 		return (
 			<div>
 				<h1>Hello and Welcome to Local Ghost</h1>
-				<p>This is the best damn ghost app on the web</p>
+				<Greeting />
 			</div>
 		)
 	}
