51765ae0f3efea1765d13d0d84d83b7faa4c7c09
Jamie Guay <Bidman22@Nicoles-MacBook-Air.local>
2018-12-08 10:13:22 -0700

    gradient added

--- src/components/navbar/nav1.js
+++ src/components/navbar/nav1.js
@@ -1,24 +1,24 @@
 import React from 'react';
+import "./index.css";
 
 
 class Navbar extends React.Component{
   render(){
     return(
 
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-<a href="https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/01/ghost-car.jpg" className="navbar-left"><img src={require('./ghosty1.png')} alt="BOO" width="27px"/></a>
-<a className="navbar-brand" href="https://trello.com/b/bqjihFDa/local-ghost-3000">LocalGhost3k</a>
+    <nav className="navbar navbar-expand-lg navbar-light navbar-custom">
+<a href="https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/01/ghost-car.jpg"><img src={require('./ghosty1.png')} alt="" width="27px"/></a>
 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <span className="navbar-toggler-icon"></span>
 </button>
-
+<a className="navbar-brand" href="https://trello.com/b/bqjihFDa/local-ghost-3000">LocalGhost3k</a>
 <div className="collapse navbar-collapse" id="navbarSupportedContent">
   <ul className="navbar-nav mr-auto">
     <li className="nav-item active">
-      <a className="nav-link" href="#">Tour <span className="sr-only">(current)</span></a>
+      <a className="nav-link" href="https://www.taketours.com/usa/">Tour <span className="sr-only">(current)</span></a>
     </li>
     <li className="nav-item">
-      <a className="nav-link" href="#">Sign in</a>
+      <a className="nav-link" href="https://accounts.google.com/ServiceLogin">Sign in</a>
     </li>
     <li className="nav-item dropdown">
       <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -27,7 +27,6 @@ class Navbar extends React.Component{
       <div className="dropdown-menu" aria-labelledby="navbarDropdown">
         <a className="dropdown-item" href="https://community.canvaslms.com/docs/DOC-13089-4152719730">Profile</a>
         <a className="dropdown-item" href="#">Hauntings</a>
-        <div className="dropdown-divider"></div>
         <a className="dropdown-item" href="#">Contact Us</a>
       </div>
     </li>
