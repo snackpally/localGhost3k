9205efa208c50e270893560d4454d3effc5d97e1
Jamie Guay <Bidman22@Nicoles-MacBook-Air.local>
2018-12-08 16:55:02 -0700

    Color experimentation plus links updated

--- src/components/navbar/nav1.js
+++ src/components/navbar/nav1.js
@@ -7,28 +7,22 @@ class Navbar extends React.Component{
     return(
 
     <nav className="navbar navbar-light navbar-custom">
-<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true"  aria-label="Toggle navigation">
+<button className="navbar-toggler navbar-left" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true"  aria-label="Toggle navigation">
   <span className="navbar-toggler-icon"></span>
 </button>
-<a href="https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/01/ghost-car.jpg" className="navbar-left"><img src={require('./ghosty1.png')} alt="" width="27px"/></a>
-<a className="navbar-brand" href="https://trello.com/b/bqjihFDa/local-ghost-3000">LocalGhost3k</a>
+<a className="navbar-brand" href="https://trello.com/b/bqjihFDa/local-ghost-3000"><img src={require('./ghosty2.gif')} alt="" width="54px"/> LocalGhost</a>
 <form className="form-inline my-2 my-lg-0">
 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
 <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
 </form>
+<button type="button" className="btn btn-outline-dark">Sign in</button>
 <div className="navbar-collapse collapse" id="navbarSupportedContent">
   <ul className="navbar-nav mr-auto">
-    <li className="nav-item">
-      <a className="nav-link" href="https://accounts.google.com/ServiceLogin">Sign in</a>
-    </li>
-        <li className="nav-item">
-        <a className="nav-link" href="https://community.canvaslms.com/docs/DOC-13089-4152719730">Profile</a>
+        <li className="nav-item active">
+        <a className="nav-link" href="https://community.canvaslms.com/docs/DOC-13089-4152719730">Profile<span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
         <a className="nav-link" href="#">Hauntings</a>
-        </li>
-        <li className="nav-item active">
-        <a className="nav-link" href="https://www.taketours.com/usa/">Tour <span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
         <a className="nav-link" href="#">Contact Us</a>
