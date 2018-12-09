c23150e98c5c7092719eef1edf28e1347ff7a863
Zack Viente <zviente@gmail.com>
2018-12-09 14:08:29 -0700

    formatting

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import UserForm from './components/login/login';
-import Navbar from './components/navbar/nav1';
+import React from "react";
+import ReactDOM from "react-dom";
+import UserForm from "./components/login/login";
+import Navbar from "./components/navbar/nav1";
 
 class App extends React.Component {
-	render() {
-		return (
-			<div>
-			<Navbar />
-				<UserForm />
-			</div>
-
-		)
-	}
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <UserForm />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
