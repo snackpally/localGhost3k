fa3b513096480104bb13056cbcf57f572e3413b9
aceslowman <aceslowman@gmail.com>
2018-12-11 20:42:42 -0700

    Changed img to background-image for card overlay

--- src/components/carousel/card.js
+++ src/components/carousel/card.js
@@ -7,13 +7,14 @@ import "./Cindex.css";
 
 class Card extends React.Component {
 	render() {
-
+		let background = {
+			backgroundImage: `url(${this.props.data.loc_img_link})`
+		}
 		return(
 			<div className="container">
 				<div className="row">
 					<div className="card" className="card-style">
-							<div className="card-image-overlay">
-								<img className="card-img-top" src={this.props.data.loc_img_link} alt="Card image"/>
+						<div className="card-image-overlay" style={background}>
 								<h5 className="card-title" className="ghost-name">{this.props.data.ghost_name}</h5>
 								<h3 className="title" className="place-name">{this.props.data.place_name}</h3>
 								<p className="loc-info">{this.props.data.loc_desc}</p>
