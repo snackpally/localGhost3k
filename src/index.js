20b9c043f4b59f0e9270084973c9433983c53765
Zack Viente <zviente@gmail.com>
2018-12-12 18:43:59 -0700

    style added and removed

--- src/index.js
+++ src/index.js
@@ -4,31 +4,26 @@ import LeafletMap from './components/LeafletMap.js';
 import Carousel from './components/carousel/carouselCont';
 import Navbar from './components/navbar/nav1.js';
 import style from './components/navbar/index.css';
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         {/* <div className="navbar"> */}
-//         <Navbar />
-//         {/* </div> */}
-//         {/* <div className="map"> */}
-//         {/* <h2 className="map-introduction">Montana Hauntings</h2> */}
-//         <LeafletMap />
-//         {/* </div> */}
-//         {/* <div className="carousel"> */}
-//         <h3 className="hauntings-near-you">Hauntings near you</h3>
-//         <Carousel />
-//         {/* </div> */}
-//       </div>
-//     );
-//   }
-// }
-
 class App extends React.Component {
   render() {
-    return <Carousel />;
+    return (
+      <div>
+        {/* <div className="navbar"> */}
+        <Navbar />
+        {/* </div> */}
+        {/* <div className="map"> */}
+        {/* <h2 className="map-introduction">Montana Hauntings</h2> */}
+        <LeafletMap />
+        {/* </div> */}
+        {/* <div className="carousel"> */}
+        <h3 className="hauntings-near-you">Hauntings near you</h3>
+        <Carousel />
+        {/* </div> */}
+      </div>
+    );
   }
 }
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // A few of these divs are redundent and are overriding the css rules already being called with bootstrap.
