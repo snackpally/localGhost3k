d6274cdb7ec82909513b3a72eae232bb48d63498
Zack Viente <zviente@gmail.com>
2018-12-18 20:40:41 -0700

    formating

--- src/components/Collapseable.js
+++ src/components/Collapseable.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Collapse, Button, CardBody, Card, Media } from 'reactstrap';
-import './component.css'
+import './component.css';
 
 export default class Collapseable extends Component {
   constructor(props) {
@@ -13,51 +13,45 @@ export default class Collapseable extends Component {
     this.setState({ collapse: !this.state.collapse });
   }
 
-  componentDidMount(){
-    this.setState({collapse: true})
+  componentDidMount() {
+    this.setState({ collapse: true });
   }
 
   render() {
-          console.log("PROPS", this.props.data);
+    console.log('PROPS', this.props.data);
     return (
-
       <div>
         <Collapse isOpen={this.state.collapse} toggle={this.markers} className={this.props.className} className="Collapse">
           <Media>
-                <Media left href="#">
-                  <Media object data-src="{this.props.data.loc_img}" alt="Collapse placeholder image" />
-                </Media>
-                <Media body>
-                  <Media heading>
-                    {this.props.data.place_name}
-                  </Media>
-                  <Media heading>
-                    {this.props.data.address.city}
-                  </Media>
-                  {this.props.data.loc_desc}
-                </Media>
-              </Media>
-              <Button color="secondary" size="sm">More info....</Button>
-              <Button color="danger" size="lg">Add to Profile</Button>{' '}
-              <Button color="secondary" size="lg">Add to tour</Button>
-
+            <Media left href="#">
+              <Media object data-src="{this.props.data.loc_img}" alt="Collapse placeholder image" />
+            </Media>
+            <Media body>
+              <Media heading>{this.props.data.place_name}</Media>
+              <Media heading>{this.props.data.address.city}</Media>
+              {this.props.data.loc_desc}
+            </Media>
+          </Media>
+          <Button color="secondary" size="sm">
+            More info....
+          </Button>
+          <Button color="danger" size="lg">
+            Add to Profile
+          </Button>{' '}
+          <Button color="secondary" size="lg">
+            Add to tour
+          </Button>
         </Collapse>
       </div>
     );
   }
 }
 
-
-
 // <img className="collapse-img" src={this.props.data.loc_img}/>
 // <h3 className="collapse-place_name" > {this.props.data.place_name}</h3>
 // <h4 className="collapse-city" > {this.props.data.address.city}</h4>
 // <p className="collapse-loc_desc"> {this.props.data.loc_desc}</p>
 // <h5 className="collapse-source">{this.props.data.info_source}</h5>
-
-
-
-
 
 // import React, { Component } from 'react';
 // import { Collapse, Button, CardBody, Card } from 'reactstrap';
