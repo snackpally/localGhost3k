ada041f468d193e49904f64b028798adb80598e8
sharonfuller <sharon.fuller717@gmail.com>
2019-01-03 20:58:55 -0700

    worked on getting the addHauntings to submit--not there yet

--- src/components/addHaunting/hauntingForm.js
+++ src/components/addHaunting/hauntingForm.js
@@ -28,25 +28,24 @@ export default class HauntingForm extends React.Component {
   };
 
   validData = () => {
-    const { data } = this.state;
-    const hasLocationName = data.hauntedLocationName.trim() !== '';
-    const hasCoordinates = !isNaN(data.coordinates);
-    const hasDescription = data.description.trim() !== '';
-    const hasImage = data.image.trim() !== '';
-    const hasAddress = data.address.trim() !== '';
-    const hasCity = data.city.trim() !== '';
-    const hasState = data.state.trim() !== '';
-    const hasSource = data.source.trim() !== '';
+    const hasLocationName = this.state.hauntedLocationName.trim() !== '';
+    const hasCoordinates = !isNaN(this.state.coordinates);
+    const hasDescription = this.state.description.trim() !== '';
+    const hasImage = this.state.image.trim() !== '';
+    const hasAddress = this.state.address.trim() !== '';
+    const hasCity = this.state.city.trim() !== '';
+    const hasState = this.state.state.trim() !== '';
+    const hasSource = this.state.source.trim() !== '';
 
     return hasLocationName && hasCoordinates && hasDescription && hasImage && hasAddress && hasCity && hasState;
   };
 
   formSubmitted = event => {
     event.preventDefault();
-    console.log(this.state.product);
+    console.log(this.state);
     if (this.validData()) {
-      console.log(this.state.product);
-      this.props.onFormSubmitted(this.state.data);
+      console.log(this.state);
+      this.props.onFormSubmitted(this.state);
     }
   };
 
@@ -55,58 +54,55 @@ export default class HauntingForm extends React.Component {
       <div className="hauntingForm">
         <Form onSubmit={this.formSubmitted}>
           <FormGroup>
-            <Label htmlFor="title">Add Your Haunted Location</Label>
+            <Label htmlFor="title">Add Your Haunted Location:</Label>
           </FormGroup>
           <FormGroup>
-            <Label htmlFor="exampleEmail">Name of Haunted Location</Label>
+            <Label htmlFor="exampleEmail">Name of haunted location:</Label>
             <Input onChange={this.handleChange} className="hauntedLocationName" name="hauntedLocationName" placeholder="Enter name of haunted location" required />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="title">GPS coordinates of haunted location: </Label>
           </FormGroup>
           <Row form>
             <Col md={6}>
               <FormGroup>
-                <Label htmlFor="exampleCity">Coordinates</Label>
                 <Input onChange={this.handleChange} type="text" className="city" name="latitude" id="exampleCity" placeholder="latitude" />
               </FormGroup>
             </Col>
             <Col md={4}>
               <FormGroup>
-                <Label htmlFor="state">State</Label>
                 <Input onChange={this.handleChange} type="text" name="longitude" id="longitude" placeholder="longitude" />
               </FormGroup>
             </Col>
           </Row>
           <FormGroup>
-            <Label htmlFor="coordinates">GPS coordinates of haunted location</Label>
-            <Input onChange={this.handleChange} className="coordinates" name="coordinates" placeholder="Enter latitude, longitude" required />
-          </FormGroup>
-          <FormGroup>
-            <Label htmlFor="description">History and Description of Haunting</Label>
+            <Label htmlFor="description">History and description of haunting:</Label>
             <Input onChange={this.handleChange} className="description" type="textarea" name="description" id="exampleText" required />
           </FormGroup>
           <FormGroup>
-            <Label htmlFor="exampleUrl">Image of Haunting Location</Label>
+            <Label htmlFor="exampleUrl">Image of haunting location:</Label>
             <Input onChange={this.handleChange} className="locationImage" type="url" name="image" id="exampleUrl" placeholder="http://example.com/image.jpg" required />
           </FormGroup>
           <FormGroup>
-            <Label htmlFor="exampleAddress">Address of haunted location</Label>
-            <Input onChange={this.handleChange} className="address" type="text" name="address" id="exampleAddress" placeholder="1234 Main St" />
+            <Label htmlFor="exampleAddress">Address of haunted location:</Label>
+            <Input onChange={this.handleChange} className="address" type="text" name="address" id="exampleAddress" placeholder="example: 1234 Main St" />
           </FormGroup>
           <Row form>
             <Col md={6}>
               <FormGroup>
-                <Label htmlFor="exampleCity">City</Label>
+                <Label htmlFor="exampleCity">City:</Label>
                 <Input onChange={this.handleChange} type="text" className="city" name="city" id="exampleCity" />
               </FormGroup>
             </Col>
             <Col md={4}>
               <FormGroup>
-                <Label htmlFor="state">State</Label>
+                <Label htmlFor="state">State:</Label>
                 <Input onChange={this.handleChange} type="text" name="state" id="state" />
               </FormGroup>
             </Col>
           </Row>
           <FormGroup>
-            <Label htmlFor="exampleEmail">Source of Information</Label>
+            <Label htmlFor="exampleEmail">Source of information:</Label>
             <Input onChange={this.handleChange} name="source" className="source" placeholder="Enter source" />
           </FormGroup>
           <Button>Submit</Button>
