2f2a175830bf425c98c9e57f40463febea65543f
Sydney Knox <sydneyknox@Sydneys-MacBook-Air.local>
2019-01-05 17:17:00 -0700

    Added overlay to map that disapears on click

--- src/components/Landing/landing.js
+++ src/components/Landing/landing.js
@@ -28,15 +28,30 @@ class Landing extends React.Component {
     //todo scroll transition
   }
 
+  handleModalClick = event => {
+    let modal=document.getElementById('modal');
+    if (modal.style.display==="none"){
+      modal.style.display="block";
+    }else{
+      modal.style.display="none";
+    }
+  }
+
   render() {
     return (
       <div className="Home">
         <div className="Landing">
-          <h1>Local Ghost 3000</h1>
-          <p>Your haunted location finder</p>
-          <Leaflet handleMarkerClick={this.handleMarkerClick} />
-          {this.state.currentLocation}
-          <Carousel />
+          <div className="homePageModal" id="modal" onClick={this.handleModalClick}>
+            <div className="homePageInner">
+
+              <h1>Local Ghost 3000</h1>
+              <p>Your haunted location finder</p>
+              <p>Click to explore!</p>
+            </div>
+          </div>
+        <Leaflet handleMarkerClick={this.handleMarkerClick} className="leafZIndex"/>
+        {this.state.currentLocation}
+        <Carousel />
         </div>
       </div>
     );
